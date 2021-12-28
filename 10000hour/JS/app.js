71115705d17b932857933943525f8a53f00b13c0
박유진(Park Yoojin) <93498523+yoojin-park19@users.noreply.github.com>
2021-12-28 11:11:31 +0900

    fix: 소수점 시간 수정
    3.33333등으로 나오는 소수점을 정수로만 나타나게 수정

--- 10000hour/JS/app.js
+++ 10000hour/JS/app.js
@@ -4,5 +4,5 @@ btn.addEventListener ('click', function clac() {
     let Name = document.getElementById("name").value;
     let Time = document.getElementById("time").value;
     document.getElementById("expertName").innerText = Name;
-    document.getElementById("spendTime").innerText = 1000/parseInt(Time) + '일';
+    document.getElementById("spendTime").innerText = parseInt(10000/parseInt(Time)) + '일';
 })
